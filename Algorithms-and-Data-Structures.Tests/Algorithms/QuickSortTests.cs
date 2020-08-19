37c7b6735271e59dedec523e9f54d537c2744300
Jason Rae <jrrae23@gmail.com>
2020-08-19 16:29:39 -0400

    Hoare method for quicksort

--- Algorithms-and-Data-Structures.Tests/Algorithms/QuickSortTests.cs
+++ Algorithms-and-Data-Structures.Tests/Algorithms/QuickSortTests.cs
@@ -5,8 +5,17 @@ namespace Algorithms_and_Data_Structures
     using Algorithms_and_Data_Structures;
     using Xunit;
 
+    /// <summary>
+    /// Tests for the Quicksort class.
+    /// </summary>
     public class QuickSortTests
     {
+        /// <summary>
+        /// Tests the LomutoPartition method using various inputs.
+        /// </summary>
+        /// <param name="data">The data to test.</param>
+        /// <param name="expected">The expected data after partitioning.</param>
+        /// <param name="expectedPivotIndex">The expected index of the pivot value.</param>
         [Theory]
         [InlineData(new int[] {3,7,8,5,2,1,9,5,4}, new int[] {3,2,1,4,7,8,9,5,5}, 3)]
         [InlineData(new int[] {8,7,6,5,4,3,2,1}, new int[] {1,7,6,5,4,3,2,8}, 0)]
@@ -19,9 +28,15 @@ namespace Algorithms_and_Data_Structures
             Assert.True(expectedPivotIndex == finalPivotIndex, $"The index of the final pivot position is incorrect. The expected was {expectedPivotIndex}, but instead it was {finalPivotIndex}");
         }
 
+        /// <summary>
+        /// Tests the HoarePartition method using various input.
+        /// </summary>
+        /// <param name="data">The data to test.</param>
+        /// <param name="expected">The expected data after partitioning.</param>
+        /// <param name="expectedPivotIndex">The expected index of the partitioned.</param>
         [Theory]
-        [InlineData(new int[] {3,7,8,5,2,1,9,5,4}, new int[] {3,4,1,2,5,8,9,5,7}, 3)]
-        [InlineData(new int[] {8,7,6,5,4,3,2,1}, new int[] {1,2,3,5,4,6,7,8}, 2)]
+        [InlineData(new int[] {3,7,8,5,2,1,9,5,4}, new int[] {1,2,8,5,7,3,9,5,4}, 1)]
+        [InlineData(new int[] {8,7,6,5,4,3,2,1}, new int[] {1,2,3,4,5,6,7,8}, 3)]
         public void HoarePartition_VariousData_ReturnsTrue(int[] data, int[] expected, int expectedPivotIndex)
         {
             var quickSort = new QuickSort();
@@ -31,13 +46,34 @@ namespace Algorithms_and_Data_Structures
             Assert.True(expectedPivotIndex == finalPivotIndex, $"The index of the final pivot position is incorrect. The expected was {expectedPivotIndex}, but instead it was {finalPivotIndex}");
         }
 
+        /// <summary>
+        /// Tests the LomutoSort method.
+        /// </summary>
+        /// <param name="data">The data to test.</param>
+        /// <param name="expected">The expected sorted data.</param>
         [Theory]
         [InlineData(new int[] {3,7,8,5,2,1,9,5,4}, new int[] {1,2,3,4,5,5,7,8,9})]
         [InlineData(new int[] {8,7,6,5,4,3,2,1}, new int[] {1,2,3,4,5,6,7,8})]
-        public void Sort_VariousData_ReturnsTrue(int[] data, int[] expected)
+        public void LomutoSort_VariousData_ReturnsTrue(int[] data, int[] expected)
         {
             var quickSort = new QuickSort();
-            quickSort.Sort(data, 0, data.Length-1);
+            quickSort.LomutoSort(data, 0, data.Length-1);
+
+            Assert.True(data.SequenceEqual(expected), $"QuickSort did not sort the data correctly. The result was {string.Join(',', data)}, but the expected was {string.Join(',',expected)}");
+        }
+
+        /// <summary>
+        /// Tests the HoareSort method.
+        /// </summary>
+        /// <param name="data">The data to test.</param>
+        /// <param name="expected">The expected sorted data.</param>
+        [Theory]
+        [InlineData(new int[] {3,7,8,5,2,1,9,5,4}, new int[] {1,2,3,4,5,5,7,8,9})]
+        [InlineData(new int[] {8,7,6,5,4,3,2,1}, new int[] {1,2,3,4,5,6,7,8})]
+        public void HoareSort_VariousData_ReturnsTrue(int[] data, int[] expected)
+        {
+            var quickSort = new QuickSort();
+            quickSort.HoareSort(data, 0, data.Length-1);
 
             Assert.True(data.SequenceEqual(expected), $"QuickSort did not sort the data correctly. The result was {string.Join(',', data)}, but the expected was {string.Join(',',expected)}");
         }
--- Algorithms-and-Data-Structures/Algorithms/QuickSort.cs
+++ Algorithms-and-Data-Structures/Algorithms/QuickSort.cs
@@ -2,25 +2,54 @@ namespace Algorithms_and_Data_Structures
 {
     using System;
 
+    /// <summary>
+    /// Quick sort implementation with choice of Lomuto or Hoare partition.
+    /// Best-case, average-case, and worst-case will theoretically be N log N.
+    /// Hoare partition will be more efficient than Lomuto's partition.
+    /// </summary>
     public class QuickSort
     {
-        public void Sort(int[] array, int low, int high)
+        /// <summary>
+        /// Sort the items using the Lomuto method of partitioning.          
+        /// Degrades to O(N^2) with already sorted data or data where all elements match.
+        /// </summary>
+        /// <param name="array">The array to sort.</param>
+        /// <param name="low">The low index value.</param>
+        /// <param name="high">The high index value.</param>
+        public void LomutoSort(int[] array, int low, int high)
         {
             if (low < high)
             {
-                var p = this.HoarePartition(array, low, high);
-                this.Sort(array, 0, p-1);
-                this.Sort(array, p+1, high);
+                var p = this.LomutoPartition(array, low, high);
+                this.LomutoSort(array, low, p-1);
+                this.LomutoSort(array, p+1, high);
             }
         }
 
         /// <summary>
-        /// Uses Lomuto partitioning scheme.
+        /// Sort the items using Hoare method of partitioning.
+        /// Should maintain N log N.
+        /// </summary>
+        /// <param name="array">The array to sort.</param>
+        /// <param name="low">The low index of the array.</param>
+        /// <param name="high">The high index of the array.</param>
+        public void HoareSort(int[] array, int low, int high)
+        {
+            if (low < high)
+            {
+                var p = this.HoarePartition(array, low, high);
+                this.HoareSort(array, low, p);
+                this.HoareSort(array, p+1, high);
+            }
+        }
+
+        /// <summary>
+        /// Uses Lomuto partitioning scheme. Uses the last item as the pivot.
         /// </summary>
         /// <param name="array">The array to partition.</param>
         /// <param name="low">The low value.</param>
         /// <param name="high">The high value.</param>
-        /// <returns>Returns the final position of the pivot value.</returns>
+        /// <returns>Returns the final index of the pivot value.</returns>
         public int LomutoPartition(int[] array, int low, int high) // treat high as pivot
         {
             int pivot = array[high];
@@ -33,26 +62,26 @@ namespace Algorithms_and_Data_Structures
                 if (array[j] < pivot)
                 {
                     // swap
-                    this.Swap(ref array[i], ref array[j]);
+                    this.Swap(array, i, j);
                     i++;
                 }
             }
             
-            this.Swap(ref array[i], ref array[high]);
+            this.Swap(array, i, high);
             return i;
         }
 
+        /// <summary>
+        /// Uses the Hoare method of partitioning. This does not degrade to O(N^2) for sorted data or data with matching elements
+        /// as long as the middle item is used as the pivot.
+        /// </summary>
+        /// <param name="array">The array to partition.</param>
+        /// <param name="low">The low index.</param>
+        /// <param name="high">The high index.</param>
+        /// <returns>Returns the final index of the pivot value.</returns>
         public int HoarePartition(int[] array, int low, int high)
         {
-            // 3,7,8,5,2,1,9,5,4
-            // pivot = 4
-            // i = -1
-            // j = 9
-            // 
-
-
-
-            int pivot = (low + high)/2;
+            int pivot = array[(low + high)/2];
             int i = low - 1;
             int j = high + 1;
             while(true)
@@ -71,15 +100,21 @@ namespace Algorithms_and_Data_Structures
                 {
                     return j;
                 }
-                this.Swap(ref array[i], ref array[j]);
+                this.Swap(array, i, j);
             }
         }
 
-        private void Swap(ref int value1, ref int value2)
+        /// <summary>
+        /// Swaps the values at two indices in the provided array.
+        /// </summary>
+        /// <param name="array">The array for which to swap values.</param>
+        /// <param name="i">An index of the array.</param>
+        /// <param name="j">An index of the array.</param>
+        private void Swap(int[] array, int i, int j)
         {
-            var temp = value1;
-            value1 = value2;
-            value2 = temp;
+            var temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
         }
     }
 }
